9ff48fb74625ea1d1e78f7c79c7463e27974a5c2
Nick Mathewson <nickm@torproject.org>
2008-11-16 21:07:28 +0000

    Add current mirror and master key from updates.torproject.org
    git-svn-id: file:///home/or/svnrepo/updater/trunk@17291 55e972cd-5a19-0410-ae62-a4d7a52db4cd

--- lib/thandy/master_keys.py
+++ lib/thandy/master_keys.py
@@ -1,5 +1,19 @@
 # Copyright 2008 The Tor Project, Inc.  See LICENSE for licensing information.
 
 MASTER_KEYS = [
-
+    {
+        "_keytype": "rsa",
+        "e": "AQAB",
+        "n": "0qZkVHg5yQE6KjIMUFH8lN9gKG8QKXiSuY9uFUXvPd7cNme/9/LpCyXJAEmxtRtqOR/mMudpndpiGlwQySP42lv75Kfmz9bWuZrNKHUW4XocSU/orj8tDcvx1DUb2KPTDxmnSOULtI4phfshBuokQSHCPmM8jB2u+EnsmoY2kf4xbsH9wou6LTTItbAe0ZwsYVB40BCmulRMx84sd+jnV5XJah9oJUoQY2L6l4UtwMQjB4FogWLiZBw+wMFDGVb/+r32c/e1qwi4B6Hxmg8YsipwNgbWS6yYXyBQg4pN4DyOjld5xSbYqTZQuHtWPZN/ttbb0xLwla1JK8/P16f6vQ",
+        "roles": [ [ "master", "/meta/keys.txt" ] ]
+   }
 ]
+
+DEFAULT_MIRRORLIST = {
+  "mirrors": [ {'name'     :"master repository",
+                'urlbase'  :"http://updates.torproject.org/",
+                'contents' : ["/**"],
+                'weight'   : 1
+                }
+             ],
+}
